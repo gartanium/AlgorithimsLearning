8e80387cc284b742301cb0aacebc0b1b81104eb7
Matthew Paul Brown <mpbhome1@gmail.com>
2016-10-03 16:28:52 -0700

    Added a tested-working-class of DepthFirstPaths

---
+++ Algorithims/GraphTheory/DepthFirstPaths.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithims.GraphTheory
+{
+    public class DepthFirstPaths // Expands upon Depth First Search
+    {
+        private bool[] mMarked; // Has dfs() been called for this vertex?
+        private int[] mEdgeTo; // Last vertex on known path to this vertex. Or in other words,
+                                //The parent edge of a given vertex... where edgeto[w] = v. 
+        private int mS; // source (Start position).
+                
+        /// <summary>
+        /// Find paths in G from source s
+        /// </summary>
+        /// <param name="G"></param>
+        /// <param name="s">Source of Path (Or start position)</param>
+        public DepthFirstPaths(Graph G, int s)
+        {
+            mMarked = new bool[G.V];
+            mEdgeTo = new int[G.V];
+            this.mS = s;
+            DFS(G, s);
+
+        }
+
+        private void DFS(Graph G, int v)
+        {
+            // First mark the vertex
+            mMarked[v] = true;
+            foreach(int w in G[v])
+            {
+                if(!mMarked[w])
+                {
+                    mEdgeTo[w] = v; // Set the parent vertex (or edge) of w to be v. It's origin. 
+                    DFS(G, w); // Recursion.
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if there is a path from s to v.    
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public bool HasPathTo(int v)
+        {
+            return mMarked[v];
+        }
+
+        public Stack<int> PathTo(int v) // Returns a stack containing the path of a given vertex to the source. 
+        {
+            Stack<int> path = new Stack<int>();
+            if (HasPathTo(v))
+            {
+                for (int i = v; i != mS; i = mEdgeTo[i])
+                    path.Push(i);
+                path.Push(mS);
+            }
+            else
+                return null;
+
+            return path;
+        }
+        
+    }
+}
---
+++ AlgorithimsTests/GraphTheory/DepthFirstPathsTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithims.GraphTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithims.GraphTheory.Tests
+{
+    [TestClass()]
+    public class DepthFirstPathsTests
+    {
+        [TestMethod()]
+        public void HasPathToTest() // Assert that given Vertices are ether in the graph or not.
+        {
+            Graph G = new Graph(11);
+            DepthFirstPaths testObject;
+
+            G.AddEdge(0, 1); // vertices 0 through 8 are in the graph. 9 and 10 are not. 
+            G.AddEdge(0, 2);
+            G.AddEdge(2, 3);
+            G.AddEdge(3, 5);
+            G.AddEdge(2, 4);
+            G.AddEdge(4, 6);
+            G.AddEdge(6, 7);
+            G.AddEdge(6, 8);
+            G.AddEdge(9, 10);
+
+            testObject = new DepthFirstPaths(G, 0);
+
+            // First assert that a graph has a path to it.
+            for (int i = 0; i < 9; i++) // 0 through 8
+            {
+                Assert.IsTrue(testObject.HasPathTo(i), i.ToString());
+            }
+
+
+            // Second assert that a graph does not have a path to it.
+            Assert.IsFalse(testObject.HasPathTo(9)); //9 and 10
+            Assert.IsFalse(testObject.HasPathTo(10));
+
+        }
+
+        [TestMethod()]
+        public void PathToTest()
+        {
+            Graph G = new Graph(11);
+            DepthFirstPaths testObject;
+
+            G.AddEdge(0, 1);
+            G.AddEdge(0, 2);
+            G.AddEdge(2, 3);
+            G.AddEdge(3, 5);
+            G.AddEdge(2, 4);
+            G.AddEdge(4, 6);
+            G.AddEdge(6, 7);
+            G.AddEdge(6, 8);
+            G.AddEdge(9, 10);
+
+            testObject = new DepthFirstPaths(G, 0);
+
+            // Expected Data.
+            int[] ePath1 = { 0, 2, 3, 5 };
+
+
+            Stack<int> sPath1 = testObject.PathTo(5);
+
+            int[] aPath1 = sPath1.ToArray();
+
+            for(int i = 0; i < aPath1.Length; i++)
+            {
+                Assert.AreEqual(ePath1[i], aPath1[i], "Expected: " + ePath1[i].ToString() + " Actual: " + aPath1[i].ToString());
+            }
+          
+        }
+    }
+}
